b711d1189d0895aaf4f2277b588ba40eb12748ed
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:20:37 -0400

    feat: add GetFlightById

---
+++ src/Application/Flights/Queries/GetFlightById/GetFlightByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Flights.Dtos;
+using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Flights.Queries.GetFlightById;
+
+public record GetFlightByIdQuery(Guid Id) : IRequest<Result<FlightResponse>>;
---
+++ src/Application/Flights/Queries/GetFlightById/GetFlightByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Application.Flights.Dtos;
+using Domain.Common.Models;
+using Domain.Flights;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Flights.Queries.GetFlightById;
+
+public class GetFlightByIdQueryHandler(IAppDbContext _context) : IRequestHandler<GetFlightByIdQuery, Result<FlightResponse>>
+{
+	public async Task<Result<FlightResponse>> Handle(GetFlightByIdQuery request, CancellationToken cancellationToken)
+	{
+		var flight = await _context
+			.Set<Flight>()
+			.FirstOrDefaultAsync(flight => flight.Id == request.Id,
+			cancellationToken);
+
+		if (flight is null)
+		{
+			return Error.NotFound(description: "Flight not found.");
+		}
+
+		return FlightResponse.From(flight);
+	}
+}
