e02ddd87b37d1db88b61012bbbab9901e9cb405e
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:19:54 -0400

    feat: add GetFlights

---
+++ src/Application/Flights/Queries/GetFlights/GetFlightsQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Flights.Dtos;
+using MediatR;
+
+namespace Application.Flights.Queries.GetFlights;
+
+public record GetFlightsQuery(string? Status) : IRequest<IReadOnlyList<FlightResponse>>;
---
+++ src/Application/Flights/Queries/GetFlights/GetFlightsQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Interfaces;
+using Application.Flights.Dtos;
+using Domain.Flights;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Flights.Queries.GetFlights;
+
+public class GetFlightsQueryHandler(IAppDbContext _dbContext)
+	: IRequestHandler<GetFlightsQuery, IReadOnlyList<FlightResponse>>
+{
+	public async Task<IReadOnlyList<FlightResponse>> Handle(GetFlightsQuery request, CancellationToken cancellationToken)
+	{
+		var status = FligthStatus.FromName(
+			name: request.Status ?? FligthStatus.Scheduled.Name,
+			ignoreCase: true);
+
+		var flights = await _dbContext
+			.Set<Flight>()
+			.AsNoTracking()
+			.Where(flight => flight.Status == status)
+			.Select(flight => FlightResponse.From(flight))
+			.ToListAsync(cancellationToken);
+
+		return flights;
+	}
+}
