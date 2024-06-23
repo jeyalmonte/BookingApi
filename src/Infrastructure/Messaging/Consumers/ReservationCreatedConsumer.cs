f1e88ac3e043acfcd54b0de2fa31ef7a37ee052e
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:41:46 -0400

    feat: Add ReservationCreatedConsumer
    *This consumer  is for sending email when a reservation is created

---
+++ src/Infrastructure/Messaging/Consumers/ReservationCreatedConsumer.cs
@@ -0,0 +1,58 @@
+﻿using Application.Common.Helpers;
+using Application.Common.Interfaces;
+using Application.Reservations.Events;
+using Domain.Reservations;
+using Infrastructure.Persistence.Context;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Messaging.Consumers;
+
+public class ReservationCreatedConsumer(
+	AppDbContext _context,
+	IEmailService _emailService,
+	ILogger<ReservationCreatedEvent> _logger)
+	: IConsumer<ReservationCreatedEvent>
+{
+	public async Task Consume(ConsumeContext<ReservationCreatedEvent> context)
+	{
+		var reservation = await _context
+			.Set<Reservation>()
+			.AsNoTracking()
+			.Include(Reservation => Reservation.Flight)
+			.SingleOrDefaultAsync(x => x.Id == context.Message.ReservationId,
+			context.CancellationToken);
+
+		if (reservation is null)
+		{
+			_logger.LogError("Reservation with {ReservationId} not found", context.Message.ReservationId);
+			return;
+		}
+
+		var properties = new Dictionary<string, string>
+		{
+			{ "RESERVATION_ID", reservation.Id.ToString() },
+			{ "PASSENGER_NAME", reservation.BookerName },
+			{ "DEPARTURE_TIME", reservation.Flight.DepartureTime.ToString() },
+			{ "ORIGIN", reservation.Flight.Origin },
+			{ "DESTINATION", reservation.Flight.Destination },
+			{ "SEAT_NUMBER", reservation.SeatNumber.ToString() }
+		};
+
+		var htmlContent = EmailHelper.GetTemplate("reservation-confirmation", properties);
+
+		var IsEmailSent = await _emailService.SendAsync(
+			to: reservation.BookerEmail,
+			subject: "Reservation Confirmation",
+			content: htmlContent,
+			cancellation: context.CancellationToken);
+
+		if (!IsEmailSent)
+		{
+			_logger.LogError("Failed to send reservation confirmation email: {ReservationId}", reservation.Id);
+		}
+
+		_logger.LogInformation("Reservation confirmation email sent successfully.");
+	}
+}
