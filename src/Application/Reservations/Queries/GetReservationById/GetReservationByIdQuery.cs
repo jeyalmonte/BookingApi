137d62e3cdb313841b10b15ea097fd07f648e151
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:29:52 -0400

    feat: Add GetReservationById

---
+++ src/Application/Reservations/Queries/GetReservationById/GetReservationByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Reservations.Dtos;
+using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Reservations.Queries.GetReservationById;
+
+public record GetReservationByIdQuery(Guid Id) : IRequest<Result<ReservationResponse>>;
---
+++ src/Application/Reservations/Queries/GetReservationById/GetReservationByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces;
+using Application.Reservations.Dtos;
+using Domain.Common.Models;
+using Domain.Reservations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Reservations.Queries.GetReservationById
+{
+	public class GetReservationByIdQueryHandler(IAppDbContext _context)
+		: IRequestHandler<GetReservationByIdQuery, Result<ReservationResponse>>
+	{
+		public async Task<Result<ReservationResponse>> Handle(GetReservationByIdQuery request, CancellationToken cancellationToken)
+		{
+			var reservation = await _context
+				.Set<Reservation>()
+				.AsNoTracking()
+				.Include(reservation => reservation.Flight)
+				.SingleOrDefaultAsync(reservation => reservation.Id == request.Id,
+				cancellationToken);
+
+			if (reservation is null)
+			{
+				return Error.NotFound(description: "Reservation not found.");
+			}
+
+			return ReservationResponse.From(reservation);
+		}
+	}
+
+}
