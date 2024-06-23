3e6a07ecd5df5733cd10941ddcf9522a1071812a
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:29:32 -0400

    feat: add CancelReservation

---
+++ src/Application/Reservations/Commands/CancelReservation/CancelReservationCommand.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Reservations.Commands.CancelReservation;
+
+public record CancelReservationCommand(Guid Id) : IRequest<Result<Guid>>;
---
+++ src/Application/Reservations/Commands/CancelReservation/CancelReservationCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Domain.Common.Models;
+using Domain.Reservations;
+using MediatR;
+
+namespace Application.Reservations.Commands.CancelReservation
+{
+	public class CancelReservationCommandHandler(IAppDbContext _context)
+		: IRequestHandler<CancelReservationCommand, Result<Guid>>
+	{
+		public async Task<Result<Guid>> Handle(CancelReservationCommand request, CancellationToken cancellationToken)
+		{
+			var reservation = await _context
+				.Set<Reservation>()
+				.FindAsync([request.Id], cancellationToken);
+
+			if (reservation is null)
+			{
+				return Error.NotFound("Reservation not found.");
+			}
+
+			reservation.CancelReservation();
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return reservation.Id;
+		}
+	}
+}
