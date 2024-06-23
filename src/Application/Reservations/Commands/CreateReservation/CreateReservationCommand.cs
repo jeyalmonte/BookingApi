5add8d99269a48d3959aab551ed542bd1d8352db
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:23:10 -0400

    feat: add CreateReservation

---
+++ src/Application/Reservations/Commands/CreateReservation/CreateReservationCommand.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common.Models;
+using MediatR;
+
+namespace Application.Reservations.Commands.CreateReservation;
+
+public record CreateReservationCommand(
+	string PassengerName,
+	string PassengerEmail,
+	Guid FlightId,
+	int SeatNumber) : IRequest<Result<Guid>>;
---
+++ src/Application/Reservations/Commands/CreateReservation/CreateReservationCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using Application.Common.Interfaces;
+using Domain.Common.Models;
+using Domain.Flights;
+using Domain.Reservations;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Reservations.Commands.CreateReservation;
+
+public class CreateReservationCommandHandler(IAppDbContext _context)
+	: IRequestHandler<CreateReservationCommand, Result<Guid>>
+{
+	public async Task<Result<Guid>> Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+	{
+		var flight = await _context
+			.Set<Flight>()
+			.Include(flight => flight.Seats)
+			.SingleOrDefaultAsync(x => x.Id == request.FlightId,
+			 cancellationToken: cancellationToken);
+
+		if (flight is null)
+		{
+			return Error.Failure(description: "Flight not found.");
+		}
+
+		if (flight.SeatsAvailable <= 0)
+		{
+			return Error.Failure(description: "Not enough available seats.");
+		}
+
+		int seat = request.SeatNumber;
+
+		if (!IsSeatAvailable(flight, ref seat))
+		{
+			return Error.Failure(description: "Seat not available.");
+		}
+
+		var reservation = Reservation.Create(
+			request.PassengerName,
+			request.PassengerEmail,
+			request.FlightId,
+			seat);
+
+		_context.Set<Reservation>().Add(reservation);
+
+		await _context.SaveChangesAsync(cancellationToken);
+
+		return reservation.Id;
+	}
+
+	private static bool IsSeatAvailable(Flight flight, ref int seatNumber)
+	{
+		if (seatNumber > 0)
+		{
+			return flight.ReserveSeat(seatNumber);
+		}
+
+		seatNumber = flight.ReserveAnySeat();
+		return seatNumber > 0;
+	}
+}
