d70a9562f7358dfbbd54add2b68f3aa4c2587265
Jeyson Almonte <jjalmonte@humano.com.do>
2024-06-23 02:18:04 -0400

    feat: add PublishDomainEvent and InsertOutboxMessage interceptors

---
+++ src/Infrastructure/Persistence/Interceptors/InsertOutboxMessageInterceptor.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Common;
+using Infrastructure.Messaging.Outbox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Persistence.Interceptors;
+
+public sealed class InsertOutboxMessageInterceptor : SaveChangesInterceptor
+{
+	public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+		DbContextEventData eventData,
+		InterceptionResult<int> result,
+		CancellationToken cancellationToken = default)
+	{
+		if (eventData.Context is not null)
+		{
+			InsertMessages(eventData.Context);
+		}
+
+		return base.SavingChangesAsync(eventData, result, cancellationToken);
+	}
+
+	private static void InsertMessages(DbContext context)
+	{
+		var domainEvents = context
+			.ChangeTracker.Entries<IEntity>()
+			.SelectMany(x => x.Entity.PopDomainEvents())
+			.ToList();
+
+		foreach (var domainEvent in domainEvents)
+		{
+			var outboxMessage = new OutboxMessage(
+				domainEvent.GetType().Name,
+				JsonConvert.SerializeObject(
+					domainEvent,
+					new JsonSerializerSettings
+					{
+						TypeNameHandling = TypeNameHandling.All
+					})
+				);
+
+			context.Add(outboxMessage);
+		}
+
+	}
+}
---
+++ src/Infrastructure/Persistence/Interceptors/PublishDomainEventsInterceptor.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Infrastructure.Persistence.Interceptors;
+
+public sealed class PublishDomainEventsInterceptor(IPublisher _publisher) : SaveChangesInterceptor
+{
+	public override async ValueTask<int> SavedChangesAsync(
+		SaveChangesCompletedEventData eventData,
+		int result,
+		CancellationToken cancellationToken = default)
+	{
+		if (eventData.Context is not null)
+		{
+			await PublishDomainEvents(eventData.Context, cancellationToken);
+		}
+
+		return await base.SavedChangesAsync(eventData, result, cancellationToken);
+	}
+
+	private async Task PublishDomainEvents(DbContext context, CancellationToken cancellationToken)
+	{
+		var domainEvents = context
+			.ChangeTracker.Entries<IEntity>()
+			.SelectMany(x => x.Entity.PopDomainEvents())
+			.ToList();
+
+		foreach (var domainEvent in domainEvents)
+		{
+			await _publisher.Publish(domainEvent, cancellationToken);
+		}
+	}
+}
