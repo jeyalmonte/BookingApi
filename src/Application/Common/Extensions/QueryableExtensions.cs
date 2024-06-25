a9f41499b6f3aa1fd907cd42510127a96175ceeb
Jeyson Almonte <jeysom28@gmail.com>
2024-06-25 17:29:36 -0400

    feat: add QueryableExtensions
    * PaginatedListAsync method

---
+++ src/Application/Common/Extensions/QueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Extensions;
+
+public static class QueryableExtensions
+{
+	public static async Task<PaginatedResult<TDestination>> PaginatedListAsync<TDestination>(
+		this IQueryable<TDestination> queryable,
+		int pageNumber,
+		int pageSize,
+		CancellationToken cancellationToken = default) where TDestination : class
+	{
+		return await PaginatedResult<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize, cancellationToken);
+	}
+}
